576f8ae6506a24baf37075fbd6a736a221bd4f21
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-10-06 10:27:25 -0300

    ✨ feat(user): Adiciona estrutura de requisição e resposta para gerenciamento de usuários

--- src/controllers/createUser.go
+++ src/controllers/createUser.go
@@ -1,7 +1,18 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rlevidev/my-firstcrud-in-go/src/config/rest_err"
+	"github.com/rlevidev/my-firstcrud-in-go/src/controllers/models/request"
+)
 
 func CreateUser(ctx *gin.Context) {
+	var userRequest request.UserRequest
 
+	if err := ctx.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(fmt.Sprintf("Há alguns campos incorretos em sua requisição. Error: %s", err.Error()), nil)
+		ctx.JSON(restErr.Status, restErr)
+	}
 }
---
+++ src/controllers/models/request/userRequest.go
@@ -0,0 +1,7 @@
+package request
+
+type UserRequest struct {
+	Email    string `json:"email"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
